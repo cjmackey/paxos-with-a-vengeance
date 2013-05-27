d73b6e3702bd92bfc476acc70e94dd413fd3bb27
Carl Mackey <carl@animoto.com>
2013-05-27 03:10:14 -0400

    bad action test

--- src/Tests/ActionTests.hs
+++ src/Tests/ActionTests.hs
@@ -17,6 +17,8 @@ import Data.ModelTree
 
 {-# ANN module "HLint: ignore Use camelCase" #-}
 
+case_no_action = runAction "Not An Action" [] Data.ModelTree.empty @?= Left "no such action!? Not An Action"
+
 case_example_return = runAction "Action.Example" [] Data.ModelTree.empty @?= Right (MInteger 42, Data.ModelTree.empty, S.empty)
 
 case_example_write = runAction "Example" [MText (T.pack "write")] Data.ModelTree.empty @?= Right (MNothing, ModelTree MNothing (M.fromList [(T.pack "some", ModelTree MNothing (M.fromList [(T.pack "path", ModelTree (MInteger 42) M.empty)]))]), S.fromList [InsModel (toTreePath "some/path") (MInteger 42)])

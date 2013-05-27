25882580f5420f2650ce68488ec8eceeb2b95493
Carl Mackey <carl@animoto.com>
2013-05-26 22:27:14 -0400

    some more modeltree stuff

--- src/Data/ModelTree.hs
+++ src/Data/ModelTree.hs
@@ -1,18 +1,69 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric, TypeSynonymInstances, FlexibleInstances #-}
 module Data.ModelTree where
 
+import Prelude hiding (lookup)
 import Control.Monad(forM, forM_)
 import Data.Serialize
 import Data.Text(Text)
+import qualified Data.Text as T
 import Data.ByteString(ByteString)
 import qualified Data.Text.Encoding
 import qualified Data.Map as M
 import Test.QuickCheck(Arbitrary)
 import GHC.Generics (Generic)
-import Model
+import Model(Model(MNothing))
 
 data ModelTree = ModelTree Model (M.Map Text ModelTree)
     deriving (Eq, Ord, Show, Generic)
+empty = ModelTree MNothing M.empty
+
+lookup :: (TreePathLike p) => p -> ModelTree -> Maybe ModelTree
+lookup p0 = l (toTreePath p0)
+  where l [] mt = Just mt
+        l (h:p) (ModelTree _ children) = M.lookup h children >>= l p
+--          case M.lookup h children of
+--            Just child -> l p child
+--            Nothing -> Nothing
+
+lookup' :: (TreePathLike p) => p -> ModelTree -> Model
+lookup' p mt = case lookup p mt of
+                 Just (ModelTree m _) -> m
+                 Nothing -> MNothing
+insert :: (TreePathLike p) => p -> Model -> ModelTree -> ModelTree
+insert p m = applyTreeMod (treeMod p m)
+
+applyTreeMod :: TreeMod -> ModelTree -> ModelTree
+applyTreeMod ([], m) (ModelTree _ children) = ModelTree m children
+applyTreeMod (p0:p, m) (ModelTree m0 children) =
+  let child = case M.lookup p0 children of
+                Just child0 -> child0
+                Nothing -> empty
+      child' = applyTreeMod (p, m) child
+      children' = M.insert p0 child' children
+  in ModelTree m0 children'
+
+type TreePath = [Text]
+type TreeMod = (TreePath, Model)
+
+treeMod :: (TreePathLike p) => p -> Model -> TreeMod
+treeMod p m = (toTreePath p, m)
+
+class TreePathLike a where
+  toTreePath :: a -> TreePath
+
+instance TreePathLike TreePath where
+  toTreePath = id
+
+instance TreePathLike [String] where
+  toTreePath = map T.pack
+
+instance TreePathLike Text where
+  toTreePath t = filter (\x -> T.length x > 0) $ T.splitOn (T.pack "/") t
+
+instance TreePathLike String where
+  toTreePath = toTreePath . T.pack
+
+
 
 instance Serialize ModelTree where
   put (ModelTree model childmap) = do
--- src/Tests/Data/ModelTreeTests.hs
+++ src/Tests/Data/ModelTreeTests.hs
@@ -7,6 +7,7 @@ import Test.Framework.Providers.QuickCheck2
 import Test.HUnit
 import Test.QuickCheck()
 
+import Prelude hiding (lookup)
 import Data.Serialize
 import qualified Data.Map as M
 import qualified Data.Text as T
@@ -20,8 +21,21 @@ recode x = decode (encode (x :: ModelTree))
 recode_eq x = recode x == Right x
 recode_ x = recode x @?= Right x
 
-case_serial_simple = recode_ (ModelTree MNothing M.empty)
-case_serial_complex = recode_ (ModelTree (MText $ T.pack "lol") (M.fromList [(T.pack "asdf", ModelTree (MInteger 17) M.empty)]))
+complexModelTree = (ModelTree (MText $ T.pack "lol") (M.fromList [(T.pack "asdf", ModelTree (MInteger 17) M.empty)]))
 
+case_serial_simple = recode_ (ModelTree MNothing M.empty)
+case_serial_complex = recode_ complexModelTree
+
+case_tree_path_string = toTreePath "/asdf/blah/hello" @?= map T.pack ["asdf","blah","hello"]
+case_tree_path_string2 = toTreePath "asdf/blah/hello" @?= map T.pack ["asdf","blah","hello"]
+
+case_applyTreeMod_0 = applyTreeMod (treeMod "" (MInteger 5)) empty @?= ModelTree (MInteger 5) M.empty
+
+case_lookup_0 = lookup "" empty @?= Just empty
+case_lookup_1 = lookup "asdf" empty @?= Nothing
+case_lookup_1_1 = lookup "" complexModelTree @?= Just complexModelTree
+case_lookup_1_1' = lookup' "" complexModelTree @?= (MText $ T.pack "lol")
+case_lookup_2 = lookup "asdf" complexModelTree @?= Just (ModelTree (MInteger 17) M.empty)
+case_lookup_3 = lookup "asdf/blah3" complexModelTree @?= Nothing
 
 testGroup = $(testGroupGenerator)
